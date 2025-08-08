a1cce8204c60893190d2b1e5229ee89dae70830f
Louis DECRAEMER <84011265+louisdcmr@users.noreply.github.com>
2025-08-08 15:19:54 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,19 +5,11 @@ const Header = () => (
         <h1 className="text-4xl font-bold my-8 w-full flex items-center gap-2">
             <img
                 src={"/logo.svg"}
-                alt={"Jellyfin Cover Maker Logo"}
+                alt={"JfCovers - DECRAEMER'Soft+"}
                 className="h-9"
             />
             Jellyfin Cover Maker
         </h1>
-        <a
-            href={"https://github.com/KartoffelChipss/Jellyfin-Cover-Maker"}
-            target={"_blank"}
-            rel={"noopener noreferrer"}
-            className="h-6 w-6"
-        >
-            <GitHub />
-        </a>
     </header>
 );
 
