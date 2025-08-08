531476ef6d824249d71583702634d478f7a5ac95
Louis DECRAEMER <84011265+louisdcmr@users.noreply.github.com>
2025-08-08 15:57:36 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ const Header = () => (
     <header className="flex items-center justify-between w-full">
         <h1 className="text-4xl font-bold my-8 w-full flex items-center gap-2">
             <img
-                src={"/logo.svg"}
+                src={"/DS.png"}
                 alt={"JfCovers - DECRAEMER'Soft+"}
                 className="h-9"
             />
